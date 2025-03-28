069d350db1cd00ef9528e51ecda7c24e3a4dfefa
The-Ashwani-Kumar <ashwanikumar26102001@gmail.com>
2025-03-28 22:19:26 +0530

    Maintaining folder structure

---
+++ Snippet.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define f(i,a,b)	for(ll i=a;i<=b;i++)
+#define rf(i,a,b)	for(ll i=a;i>=b;i--)
+#define fg(i,a,b,gap)	for(ll i=a;i<=b;i+=gap)
+#define v(dataType) vector<dataType>
+#define vv(dataType) vector<vector<dataType>>
+#define vp(dataType1,dataType2) vector<pair<dataType1,dataType2>>
+#define sortV(s) sort(s.begin(),s.end())
+#define sortR(s) sort(s.rbegin(),s.rend())
+using namespace std;
+
+int main(){
+    // Redirect input and output to files
+    freopen("inputf.in", "r", stdin);  // Read input from inputf.in
+    freopen("outputf.out", "w", stdout);  // Write output to outputf.out
+
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+    }
+}
